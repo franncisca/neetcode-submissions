5c9e8a9723e4f284e2f011158b341262a531cf3d
Yan LI <55560952+franncisca@users.noreply.github.com>
2026-07-02 18:05:23 +0200

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        //review1 solution with hashmap
+        //Need to sorted, doesn't 100% understand the solution
+        //but got results with duplicats 
+        //Skip too early
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int target = - nums[i];
+            Map<Integer, Integer> indices = new HashMap<>();
+            for ( int j = i + 1; j < nums.length; j++) {
+                int diff = target - nums[j];
+                  
+                if (indices.containsKey(diff)) {
+                    res.add(List.of(nums[i], diff, nums[j]));
+
+                    // Skip duplicate j
+                    while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
+                        j++;
+                    }  
+                } 
+
+                indices.put(nums[j], j);
+            }
+
+        }
+
+        return res;
+    }
+}
