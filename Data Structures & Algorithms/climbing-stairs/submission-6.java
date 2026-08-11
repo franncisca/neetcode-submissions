705f531ffc3c4fbd17fce3e57be61d64f4b04db9
Yan LI <55560952+franncisca@users.noreply.github.com>
2026-08-11 11:15:38 +0200

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        //Review 2 ways(n) = ways(n-1) + ways(n-2)
+        int stepOne = 1;
+        int stepTwo = 1;
+
+        if (n == 1) {
+            return 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            int temp = stepOne;
+            stepOne = stepTwo;
+            stepTwo = temp + stepTwo;
+        }
+
+        return stepTwo;
+    }
+}
