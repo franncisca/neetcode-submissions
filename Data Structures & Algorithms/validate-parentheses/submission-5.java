b61e3db2cb02e1c384e405734f091b545de5ef4f
Yan LI <55560952+franncisca@users.noreply.github.com>
2026-08-23 12:39:34 +0200

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isValid(String s) {
+        //Solution with arraylist
+        Map<Character, Character> parenthese = new HashMap<>();
+        parenthese.put(')', '(');
+        parenthese.put('}', '{');
+        parenthese.put(']', '[');
+
+        if (s.length()%2 != 0) {
+            return false;
+        }
+        List<Character> open = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                open.add(c);
+                System.out.println("c: " + c);
+            }
+            else {
+                if (open.isEmpty()) {
+                    return false;
+                }
+                Character openC = open.get(open.size() - 1);
+                if(! openC.equals(parenthese.get(c))) {
+                    return false;
+                } 
+                else {
+                    open.remove(openC);
+                }
+            }
+        }
+
+        if (!open.isEmpty()) {
+            return false;
+        }
+
+        return true;
+        
+    }
+}
