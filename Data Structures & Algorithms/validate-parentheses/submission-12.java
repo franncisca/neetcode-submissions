b45ae2bd5b99ed85bcf9bd4eebfbeaca91cbe03f
Yan LI <55560952+franncisca@users.noreply.github.com>
2026-09-03 17:33:44 +0200

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        //review1 solution with stack
+        Stack<Character> openStack = new Stack<>();
+        Map<Character, Character> closeToOpen = new HashMap<>();
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('}', '{');
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || 
+                c == '[' ||
+                c == '{') {
+                openStack.push(c);
+            }
+            else {
+                if (openStack.isEmpty()) {
+                    return false;
+                }
+                Character last = openStack.peek();
+                if (last != closeToOpen.get(c)) {
+                    return false;
+                }
+                openStack.pop();
+            }
+        }
+        return openStack.isEmpty();
+    }
+}
